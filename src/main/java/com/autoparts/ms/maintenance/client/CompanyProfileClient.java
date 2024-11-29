6e93d176050cf4377710caf12a8d0d2a65615749
autoparts-dev <devone.autoparts@gmail.com>
2024-11-29 23:17:53 +0800

    Merge branch 'initial' of git@github.com:autoparts-dev/autoparts-ms-maintenance.git into initial

--- src/main/java/com/autoparts/ms/maintenance/client/CompanyProfileClient.java
+++ src/main/java/com/autoparts/ms/maintenance/client/CompanyProfileClient.java
@@ -26,7 +26,7 @@ public interface CompanyProfileClient {
 	public ResponseEntity findCompanyById(@PathVariable(name = "id") String id) throws Exception ;
 	
 	@PostMapping(path = "/create", produces = "application/json", consumes = "application/json")
-	public Object create(@RequestBody CompanyCreateVO vo) throws Exception;
+	public ResponseEntity<Object> create(@RequestBody CompanyCreateVO vo) throws Exception;
 	
 	
 }
